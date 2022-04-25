1fdcf5ca56a348df841b09ba9646ebccf18b63fd
Valerie Bayard <valerie.bayard@gmail.com>
2022-04-25 17:28:23 +0100

    added in an edit page for cheeses with relevant route and cancel button

--- app/controllers/cheeses_controller.py
+++ app/controllers/cheeses_controller.py
@@ -10,7 +10,25 @@ def index():
     cheeses = cheese_repository.select_all()
     return render_template("cheeses/index.html", all_cheeses=cheeses)
 
+# @cheeses_blueprint.route("/cheeses", methods = ['POST'])
+# def index():
+#     cheeses = cheese_repository.select_all()
+#     return render_template("cheeses/index.html", all_cheeses=cheeses)
+
 @cheeses_blueprint.route("/cheeses/<id>/edit")
 def edit_cheese(id):
     cheese = cheese_repository.select(id)
     return render_template('cheeses/edit.html', cheese = cheese)
+
+@cheeses_blueprint.route("/cheeses/<id>", methods = ['POST'])
+def update_cheese(id):
+    name = request.form["name"]
+    origin = request.form["origin"]
+    type = request.form["type"]
+    description = request.form["description"]
+    stock = request.form["stock"]
+    buying_cost = request.form["buying_cost"]
+    selling_price = request.form["selling_price"]
+    cheese = Cheese(name, origin, type, description, stock, buying_cost, selling_price, id)
+    cheese_repository.update(cheese)
+    return redirect("/cheeses")
--- app/models/cheese.py
+++ app/models/cheese.py
@@ -1,11 +1,11 @@
 class Cheese():
     
-    def __init__ (self, name, origin, type, description, stock, buying_cost, selling_cost, id = None):
+    def __init__ (self, name, origin, type, description, stock, buying_cost, selling_price, id = None):
         self.name = name
         self.origin = origin
         self.type = type
         self.description = description
         self.stock = stock
         self.buying_cost = buying_cost
-        self.selling_price = selling_cost
+        self.selling_price = selling_price
         self.id = id
