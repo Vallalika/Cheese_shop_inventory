665046a47d349988330d058787399e8395673929
Valerie Bayard <valerie.bayard@gmail.com>
2022-04-25 14:13:52 +0100

    fixed stylesheet link and did basic app layout in css

--- app/app.py
+++ app/app.py
@@ -12,9 +12,9 @@ app = Flask(__name__)
 # app.register_blueprint(zombies_blueprint)
 # app.register_blueprint(zombie_types_blueprint)
 
-# @app.route("/")
-# def main():
-#     return render_template('index.html')
+@app.route("/")
+def main():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
