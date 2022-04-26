6391dd96a66e476ed1d71ba9d6c119ade65b67d0
Valerie Bayard <valerie.bayard@gmail.com>
2022-04-26 13:21:18 +0100

    created inventory template and set it as the homepage for the app

--- app/app.py
+++ app/app.py
@@ -4,6 +4,8 @@ from controllers.cheeses_controller import cheeses_blueprint
 from controllers.providers_controller import providers_blueprint
 from controllers.cheese_provisions_controller import cheese_provisions_blueprint
 
+import repositories.cheese_repository as cheese_repository
+
 app = Flask(__name__)
 
 app.register_blueprint(cheeses_blueprint)
@@ -13,7 +15,8 @@ app.register_blueprint(cheese_provisions_blueprint)
 
 @app.route("/")
 def main():
-    return render_template('index.html')
+    cheeses = cheese_repository.select_all()
+    return render_template("index.html", all_cheeses=cheeses)
 
 if __name__ == '__main__':
     app.run()
