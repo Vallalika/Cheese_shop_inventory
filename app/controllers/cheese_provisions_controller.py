e2ee44ed115ec3f6843142be9525e395f1dadc73
Valerie Bayard <valerie.bayard@gmail.com>
2022-04-27 12:29:49 +0100

    added delete functionality to  cheese_provision pages

--- app/controllers/cheese_provisions_controller.py
+++ app/controllers/cheese_provisions_controller.py
@@ -10,20 +10,52 @@ import repositories.provider_repository as provider_repository
 
 cheese_provisions_blueprint = Blueprint("cheese_provisions", __name__)
 
-# All providers by cheese
+# Show all providers by cheese
 @cheese_provisions_blueprint.route("/cheese_provisions/<cheese_id>/provisions_by_cheese")
 def provisions_by_cheese(cheese_id):
     provisions = cheese_provision_repository.select_by_cheese_id(cheese_id)
     cheese = cheese_repository.select(cheese_id)
     return render_template("cheese_provisions/provisions_by_cheese.html", provisions_by_cheese=provisions, cheese = cheese)
 
-# NEW by cheese
+# NEW provision by cheese - to select from existing providers
+@cheese_provisions_blueprint.route("/cheese_provisions/<cheese_id>/add_by_cheese")
+def add_by_cheese(cheese_id):
+    # Get all providers to the template so any of them can be selected
+    providers = provider_repository.select_all()
+    cheese = cheese_repository.select(cheese_id)
+    return render_template('cheese_provisions/add_by_cheese.html', cheese=cheese, all_providers = providers)
+
+# NEW provision by cheese - from fully new provider
 @cheese_provisions_blueprint.route("/cheese_provisions/<cheese_id>/new_by_cheese")
 def new_by_cheese(cheese_id):
     cheese = cheese_repository.select(cheese_id)
     return render_template('cheese_provisions/new_by_cheese.html', cheese=cheese)
 
-# CREATE provision by cheese
+# CREATE provision by cheese (existing cheeses)
+@cheese_provisions_blueprint.route("/cheese_provisions/by-cheese/existing-provider/<cheese_id>", methods = ["POST"])
+def add_provision_by_cheese(cheese_id):
+
+    # Getting cheese object from relevant repo
+    cheese = cheese_repository.select(cheese_id)
+
+    # Getting provider data from form
+    provider_id = request.form["provider_id"]
+
+    # Retrieving provider from db
+    provider = provider_repository.select(provider_id)
+
+    # Creating provision with cheese and provider objects, and saving it to db
+
+    cheese_provision = CheeseProvision(cheese,provider)
+    cheese_provision_repository.save(cheese_provision)
+
+    # Getting all provisions to render relevant template
+    provisions = cheese_provision_repository.select_by_cheese_id(cheese_id)
+
+    # Rendering template with all data
+    return render_template("cheese_provisions/provisions_by_cheese.html", cheese = cheese, provisions_by_cheese=provisions)
+
+# CREATE provision by cheese (fully new cheese)
 @cheese_provisions_blueprint.route("/cheese_provisions/by-cheese/<cheese_id>", methods = ["POST"])
 def create_provision_by_cheese(cheese_id):
 
@@ -57,7 +89,7 @@ def provisions_by_provider(provider_id):
     provider = provider_repository.select(provider_id)
     return render_template("cheese_provisions/provisions_by_provider.html", provisions_by_provider=provisions, provider = provider)
 
-# NEW by provider - to select from existing cheeses
+# NEW provision by provider - to select from existing cheeses
 @cheese_provisions_blueprint.route("/cheese_provisions/<provider_id>/add_by_provider")
 def add_by_provider(provider_id):
     cheeses = cheese_repository.select_all()
@@ -65,18 +97,15 @@ def add_by_provider(provider_id):
     provider_id = provider_id
     return render_template('cheese_provisions/add_by_provider.html', provider=provider, all_cheeses = cheeses)
 
-# NEW by provider - to create a completely new cheese
+# NEW provision by provider - to create a completely new cheese
 @cheese_provisions_blueprint.route("/cheese_provisions/<provider_id>/new_by_provider")
 def new_by_provider(provider_id):
     provider = provider_repository.select(provider_id)
     return render_template('cheese_provisions/new_by_provider.html', provider=provider)
 
-# CREATE provision by provider (new)
+# CREATE provision by provider (fully new cheese)
 @cheese_provisions_blueprint.route("/cheese_provisions/by-provider/<provider_id>", methods = ["POST"])
 def create_provision_by_provider(provider_id):
-
-    # # Getting all_cheeses
-    # cheeses = cheese_repository.select_all()
 
     # Getting provider object from relevant repo
     provider = provider_repository.select(provider_id)
@@ -109,7 +138,7 @@ def create_provision_by_provider(provider_id):
     # Rendering template with all data
     return render_template("cheese_provisions/provisions_by_provider.html", provider = provider, provisions_by_provider=provisions)
 
-# CREATE provision by provider (existing cheese)
+# CREATE provision by provider (existing cheeses)
 @cheese_provisions_blueprint.route("/cheese_provisions/by-provider/existing-cheese/<provider_id>", methods = ["POST"])
 def add_provision_by_provider(provider_id):
 
@@ -133,8 +162,31 @@ def add_provision_by_provider(provider_id):
     # Rendering template with all data
     return render_template("cheese_provisions/provisions_by_provider.html", provider = provider, provisions_by_provider=provisions)
 
-# # DELETE
-# @providers_blueprint.route("/providers/<id>/delete", methods=["POST"])
-# def delete_provider(id):
-#     provider_repository.delete(id)
-#     return redirect("/providers")
+# DELETE provision with provider id
+@cheese_provisions_blueprint.route("/cheese_provisions/<provider_id>/<provision_id>/delete-by-provider", methods=["POST"])
+def delete_provision_by_provider(provider_id, provision_id):
+    # Delete relevant provision
+    cheese_provision_repository.delete(provision_id)
+
+    # Get provider object to pass it along to end template
+    provider = provider_repository.select(provider_id)
+
+    # Get all provisions per provider to pass them along to end template
+    provisions = cheese_provision_repository.select_by_provider_id(provider_id)
+
+    return render_template("cheese_provisions/provisions_by_provider.html", provisions_by_provider=provisions, provider = provider)
+
+# DELETE provision with cheese id
+@cheese_provisions_blueprint.route("/cheese_provisions/<cheese_id>/<provision_id>/delete-by-cheese", methods=["POST"])
+def delete_provision_by_cheese(cheese_id, provision_id):
+    
+    # Delete relevant provision
+    cheese_provision_repository.delete(provision_id)
+
+    # Get cheese object to pass it along to end template
+    cheese = cheese_repository.select(cheese_id)
+
+    # Get all provisions per cheese to pass them along to end template
+    provisions = cheese_provision_repository.select_by_cheese_id(cheese_id)
+
+    return render_template("cheese_provisions/provisions_by_cheese.html", provisions_by_cheese=provisions, cheese = cheese)
